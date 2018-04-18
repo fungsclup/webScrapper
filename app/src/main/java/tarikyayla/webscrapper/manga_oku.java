9652a33fc5bdfb65d7dc06975fc35971596b19db
Tarık,Yayla <fungsclup33@gmail.com>
2018-04-18 22:55:04 +0300

    first cmt

---
+++ app/src/main/java/tarikyayla/webscrapper/manga_oku.java
@@ -0,0 +1,13 @@
+package tarikyayla.webscrapper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class manga_oku extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manga_oku);
+    }
+}
